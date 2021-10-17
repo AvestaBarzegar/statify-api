052edabdc838aa56a60705514bf61ec8537e1f37
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-17 01:02:00 -0400

    Configure project with necessary boilerplate to get started

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type person struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var persons = []person{
+	{ID: "1", Name: "Avesta"},
+}
+
+func mockResponse(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, persons)
+}
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello World")
+	router := gin.Default()
+	router.GET("/", mockResponse)
+
+	router.Run()
 }
