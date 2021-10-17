920fc4c66dfb0eb855bd6989f59555d5026456ee
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-17 13:04:07 -0400

    cleanup

--- server/router.go
+++ server/router.go
@@ -1,14 +1,15 @@
 package server
 
 import (
+	"os"
+
 	"github.com/AvestaBarzegar/statify-api/controllers"
-	"github.com/AvestaBarzegar/statify-api/helpers"
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.New()
-	PORT := helpers.GoDotEnvVar("PORT")
+	PORT := os.Getenv("PORT")
 	authRouter := router.Group("/v1/api/account/")
 	authRouter.GET("/login", controllers.AuthenticateUser)
 	router.Run(":" + PORT)
