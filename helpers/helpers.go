d2f61b7ecec687e8cd667b065bf80a5df83a88d8
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-17 12:59:34 -0400

    Configure docker and remove unnecessary dependency

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,19 +1,11 @@
 package helpers
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GoDotEnvVar(key string) string {
 
 	// load .env file
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 	return os.Getenv(key)
 }
