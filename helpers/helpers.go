11a5dbd475ea8ce1f1c6d98de8b288701db29fbb
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-17 13:03:41 -0400

    cleanup code and refactor

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,11 +1,29 @@
 package helpers
 
 import (
+	"net/url"
 	"os"
 )
 
-func GoDotEnvVar(key string) string {
+var SPOTIFY_CLIENT_ID = os.Getenv("SPOTIFY_CLIENT_ID")
+var SPOTIFY_REDIRECT_URI = os.Getenv("SPOTIFY_REDIRECT_URI")
 
-	// load .env file
-	return os.Getenv(key)
+func RedirectURL() string {
+	scope := "user-top-read,user-read-recently-played,user-read-private,user-read-email"
+	params := url.Values{}
+	params.Add("client_id", SPOTIFY_CLIENT_ID)
+	params.Add("response_type", "code")
+	params.Add("redirect_uri", SPOTIFY_REDIRECT_URI)
+	params.Add("scope", scope)
+	params.Add("show_dialog", "true")
+
+	query := params.Encode()
+	url := url.URL{
+		Scheme:   "https",
+		Host:     "accounts.spotify.com",
+		Path:     "authorize",
+		RawQuery: query,
+	}
+
+	return url.String()
 }
