2db989b38db58dbebfb79700edc1fc7b450848ef
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-22 00:01:21 -0400

    made the endpoint that returns lyrics dynamic

--- controllers/songs.go
+++ controllers/songs.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/AvestaBarzegar/statify-api/bindings"
 	"github.com/AvestaBarzegar/statify-api/helpers"
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +19,7 @@ import (
 
 returns:
 {
-	// The bit below is acquired from calling GET https://api.spotify.com/v1/audio-analysis?id={id}&artist={artist}&title={title}
+	// The bit below is acquired from calling GET https://api.spotify.com/v1/audio-analysis?id={id}&artist={artist}&track={track}
 	track audio analysis: {
 		"danceability": 0.847,
 		"energy": 0.529,
@@ -39,13 +43,26 @@ returns:
 // GET v1/song/{id}/analysis
 
 func GetLyrics(c *gin.Context) {
+	queryParams := bindings.SongInformationQuery{}
 
-	res, err := helpers.GetLyrics("Frank Ocean", "Nights")
-
+	err := c.ShouldBind(&queryParams)
 	if err != nil {
-		c.JSON(res.StatusCode, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println(err)
 		return
 	}
-	defer res.Body.Close()
-	c.DataFromReader(res.StatusCode, res.ContentLength, "application/json", res.Body, nil)
+
+	if queryParams.Artist == "" || queryParams.SongId == "" || queryParams.Track == "" {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	song, err := helpers.GetLyrics(queryParams.Artist, queryParams.Track)
+
+	if err != nil {
+		c.JSON(song.StatusCode, gin.H{"error": err.Error()})
+		return
+	}
+	defer song.Body.Close()
+	c.DataFromReader(song.StatusCode, song.ContentLength, "application/json", song.Body, nil)
 }
