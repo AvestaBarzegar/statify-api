271c3d048cad259c0dd84d8663144c07664b22e3
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-24 19:11:19 -0400

    configure migrations

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,40 +1,35 @@
 CREATE TYPE time_span AS ENUM('4 weeks', '6 months', 'all time');
-
 CREATE TYPE track AS (
-  track_name text NOT NULL,
-  artist_name text NOT NULL,
-  spotify_track_id text NOT NULL unique,
-  track_image_url text NOT NULL,
-  track_rank int NOT NULL
+  track_name text,
+  artist_name text,
+  spotify_track_id text,
+  track_image_url text,
+  track_rank int
 );
-
 CREATE TYPE artist AS (
-  artist_name text NOT NULL,
-  artist_spotify_id text NOT NULL,
-  artist_image_url text NOT NULL,
-  artist_rank int NOT NULL
+  artist_name text,
+  artist_spotify_id text,
+  artist_image_url text,
+  artist_rank int
 );
-
 CREATE TABLE users (
-  id int PRIMARY KEY SERIAL,
+  id int PRIMARY KEY,
   spotify_user_id text UNIQUE NOT NULL,
   created_at timestamp NOT NULL,
   is_premium boolean NOT NULL,
   updated_at timestamp NOT NULL
 );
-
 CREATE TABLE tracks (
-  id int PRIMARY KEY SERIAL,
+  id int PRIMARY KEY,
   spotify_user_id text NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   time_span time_span NOT NULL,
-  tracks track[] NOT NULL
+  tracks track [] NOT NULL
 );
-
 CREATE TABLE artists (
-  id int PRIMARY KEY SERIAL,
+  id int PRIMARY KEY,
   spotify_user_id text NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   time_span time_span NOT NULL,
-  tracks artist[] NOT NULL
+  tracks artist [] NOT NULL
 );
