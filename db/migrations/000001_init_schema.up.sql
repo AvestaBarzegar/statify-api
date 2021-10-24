3a7283a170437e3ca7bc909f019aed1a8c8afb6a
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-24 19:58:54 -0400

    finished migrations

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,4 +1,7 @@
-CREATE TYPE time_span AS ENUM('4 weeks', '6 months', 'all time');
+BEGIN;
+CREATE TYPE time_span AS ENUM('four weeks', 'six months', 'all time');
+COMMIT;
+BEGIN;
 CREATE TYPE track AS (
   track_name text,
   artist_name text,
@@ -6,30 +9,45 @@ CREATE TYPE track AS (
   track_image_url text,
   track_rank int
 );
+COMMIT;
 CREATE TYPE artist AS (
   artist_name text,
   artist_spotify_id text,
   artist_image_url text,
   artist_rank int
 );
+BEGIN;
 CREATE TABLE users (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   spotify_user_id text UNIQUE NOT NULL,
   created_at timestamp NOT NULL,
   is_premium boolean NOT NULL,
   updated_at timestamp NOT NULL
 );
+COMMIT;
+BEGIN;
 CREATE TABLE tracks (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   spotify_user_id text NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   time_span time_span NOT NULL,
-  tracks track [] NOT NULL
+  items track [] NOT NULL
 );
+COMMIT;
+BEGIN;
 CREATE TABLE artists (
-  id int PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   spotify_user_id text NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   time_span time_span NOT NULL,
-  tracks artist [] NOT NULL
+  items artist [] NOT NULL
 );
+COMMIT;
+BEGIN;
+CREATE INDEX ON items (tracks);
+CREATE INDEX ON created_at (tracks);
+COMMIT;
+BEGIN;
+CREATE INDEX ON items (artists);
+CREATE INDEX ON created_at (artists);
+COMMIT;
