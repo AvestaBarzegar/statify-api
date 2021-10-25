930879115828751f79b2aea1640755062e58d0ae
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-25 19:43:51 -0400

    create endpoint to retrive song information

--- helpers/consts/consts.go
+++ helpers/consts/consts.go
@@ -7,7 +7,7 @@ import (
 
 const SpotifyTokenURL = "https://accounts.spotify.com/api/token"
 const LyricsURL = "https://api.lyrics.ovh/v1/"
-const AudioFeaturesURL = "https://api.spotify.com/v1/audio-features"
+const AudioFeaturesURL = "https://api.spotify.com/v1/audio-features/"
 
 var ClientSecret = os.Getenv("SPOTIFY_CLIENT_SECRET")
 var ClientId = os.Getenv("SPOTIFY_CLIENT_ID")
