491180232face2c1263fd15d300a75a52ce81e6e
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-25 23:04:15 -0400

    get int of DB port

--- helpers/consts/consts.go
+++ helpers/consts/consts.go
@@ -3,6 +3,7 @@ package consts
 import (
 	b64 "encoding/base64"
 	"os"
+	"strconv"
 )
 
 // Common URLS TODO: Refactor and made methods that build URLs
@@ -16,7 +17,6 @@ var ClientId = os.Getenv("SPOTIFY_CLIENT_ID")
 
 // DB Configuration information
 var HOST = os.Getenv("DB_HOST")
-var PORT = os.Getenv("DB_PORT")
 var USER = os.Getenv("POSTGRES_USER")
 var PASSWORD = os.Getenv("POSTGRES_PASSWORD")
 var DB = os.Getenv("POSTGRES_DB")
@@ -24,4 +24,12 @@ var DB = os.Getenv("POSTGRES_DB")
 func EncodeClientSecretAndId() string {
 	toBeEncoded := ClientId + ":" + ClientSecret
 	return b64.StdEncoding.EncodeToString([]byte(toBeEncoded))
+}
+
+func GetPortDB() int64 {
+	port, err := strconv.ParseInt(os.Getenv("DB_PORT"), 0, 64)
+	if err != nil {
+		panic(err)
+	}
+	return port
 }
---
+++ models/db/connection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/AvestaBarzegar/statify-api/helpers/consts"
+)
+
+func OpenConnection() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", consts.HOST, consts.GetPortDB(), consts.USER, consts.PASSWORD, consts.DB)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
