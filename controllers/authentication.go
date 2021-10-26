de562b1e9150e04c881158779c1ddd40f7120fbc
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-26 01:09:07 -0400

    finished

--- controllers/authentication.go
+++ controllers/authentication.go
@@ -1,58 +1,37 @@
 package controllers
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
-	"github.com/AvestaBarzegar/statify-api/models/database"
+	"github.com/AvestaBarzegar/statify-api/helpers"
+	"github.com/AvestaBarzegar/statify-api/helpers/consts"
+	httpModels "github.com/AvestaBarzegar/statify-api/models/http"
 )
 
 // POST /v1/api/token
 func ProvideAccessToken(c *gin.Context) {
-	tracks := []database.Track{
-		database.Track{
-			TrackName:      "Blonde",
-			TrackImageURL:  "https://sugma",
-			TrackRank:      1,
-			SpotifyTrackId: "skajdlkajsd",
-			ArtistName:     "Frank Ocean",
-		},
-		database.Track{
-			TrackName:      "Blonde",
-			TrackImageURL:  "https://sugma",
-			TrackRank:      2,
-			SpotifyTrackId: "skajdlkajsd",
-			ArtistName:     "PNG Ocean",
-		},
-		database.Track{
-			TrackName:      "Blonde",
-			TrackImageURL:  "https://sugma",
-			TrackRank:      3,
-			SpotifyTrackId: "skajdlkajsd",
-			ArtistName:     "JPEG Ocean",
-		},
+	body := httpModels.RequestAccessTokenQuery{}
+
+	err := c.ShouldBind(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println(err)
+		return
 	}
 
-	database.InsertTrackRow("alrighticantfindagoodname", database.FourWeeks, tracks)
-	c.String(200, "testing")
-	// body := httpModels.RequestAccessTokenQuery{}
+	if body.Code == "" || body.GrantType != "authorization_code" || body.RedirectURI == consts.SpotifyTokenURL {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+	res, err := helpers.ExchangeCodeForToken(body.GrantType, body.RedirectURI, body.Code)
 
-	// err := c.ShouldBind(&body)
-	// if err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// if body.Code == "" || body.GrantType != "authorization_code" || body.RedirectURI == consts.SpotifyTokenURL {
-	// 	c.String(http.StatusBadRequest, "Bad Request")
-	// 	return
-	// }
-	// res, err := helpers.ExchangeCodeForToken(body.GrantType, body.RedirectURI, body.Code)
-
-	// if err != nil {
-	// 	c.JSON(res.StatusCode, gin.H{"error": err.Error()})
-	// 	return
-	// }
-	// defer res.Body.Close()
-	// c.DataFromReader(res.StatusCode, res.ContentLength, "application/json", res.Body, nil)
+	if err != nil {
+		c.JSON(res.StatusCode, gin.H{"error": err.Error()})
+		return
+	}
+	defer res.Body.Close()
+	c.DataFromReader(res.StatusCode, res.ContentLength, "application/json", res.Body, nil)
 }
