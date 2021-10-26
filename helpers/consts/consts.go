852d7a37674b8e5ac2a2996c3e76287d9ff21e6e
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-25 22:51:49 -0400

    add environment variables for connecting to DB

--- helpers/consts/consts.go
+++ helpers/consts/consts.go
@@ -5,12 +5,21 @@ import (
 	"os"
 )
 
+// Common URLS TODO: Refactor and made methods that build URLs
 const SpotifyTokenURL = "https://accounts.spotify.com/api/token"
 const LyricsURL = "https://api.lyrics.ovh/v1/"
 const AudioFeaturesURL = "https://api.spotify.com/v1/audio-features/"
 
+// Spotify Client Information
 var ClientSecret = os.Getenv("SPOTIFY_CLIENT_SECRET")
 var ClientId = os.Getenv("SPOTIFY_CLIENT_ID")
+
+// DB Configuration information
+var HOST = os.Getenv("DB_HOST")
+var PORT = os.Getenv("DB_PORT")
+var USER = os.Getenv("POSTGRES_USER")
+var PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+var DB = os.Getenv("POSTGRES_DB")
 
 func EncodeClientSecretAndId() string {
 	toBeEncoded := ClientId + ":" + ClientSecret
