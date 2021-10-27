6335c009620e294442b049b93a679f63c3c3af31
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-26 23:54:26 -0400

    refactor

--- models/database/inserts.go
+++ models/database/inserts.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"database/sql/driver"
-	"fmt"
 	"log"
 
 	"github.com/lib/pq"
@@ -28,16 +26,6 @@ func InsertTrackRow(spotifyUserId string, time_span string, tracks []Track) bool
 		return false
 	}
 	return true
-}
-
-func (v Track) Value() (driver.Value, error) {
-	s := fmt.Sprintf("(%s, %s, %s, %s, %d)",
-		v.TrackName,
-		v.ArtistName,
-		v.SpotifyTrackId,
-		v.TrackImageURL,
-		v.TrackRank)
-	return []byte(s), nil
 }
 
 // Note that time_span is either FourWeeks, SixMonths, or AllTime
