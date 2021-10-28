36cb3c84ec3d7e8a4e4e1bb05ae600eb0d235542
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-28 00:49:40 -0400

    fix scanners

--- models/database/model-helpers.go
+++ models/database/model-helpers.go
@@ -66,3 +66,43 @@ func (a *Artists) Scan(value interface{}) error {
 
 	return nil
 }
+
+func (t *Tracks) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("incompatible type from bytes")
+	}
+	// Make a string that is parseable
+	source := string(bytes)
+
+	// Replace all unnecessary characters
+	for _, old := range []string{"\\\"", "\"", "{", "}", "(", "[", "]"} {
+		source = strings.ReplaceAll(source, old, "")
+	}
+	var res Tracks
+	// Split up the whole string into an array of strings that represent each artist object
+	tracks := strings.Split(source, "),")
+	// Remove all unnecessary braces
+	tracks[len(tracks)-1] = strings.TrimRight(tracks[len(tracks)-1], ")")
+	// If the column is empty then we can return an empty array
+	if len(tracks) == 1 && tracks[0] == "" {
+		t = &Tracks{}
+		return nil
+	}
+	for _, track := range tracks {
+		trackRawData := strings.Split(track, ",")
+		i, err := strconv.Atoi(trackRawData[4])
+		if err != nil {
+			return fmt.Errorf("parce TrackRank raw data (%s) in %d iteration error: %v", track, i, err)
+		}
+		res = append(res, Track{
+			TrackName:      trackRawData[0],
+			ArtistName:     trackRawData[1],
+			SpotifyTrackId: trackRawData[2],
+			TrackImageURL:  trackRawData[3],
+			TrackRank:      i,
+		})
+	}
+	*t = res
+	return nil
+}
