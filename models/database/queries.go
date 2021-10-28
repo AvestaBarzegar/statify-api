8a4e89bb84947d46468eedb1720b7e0d53db72e5
Avesta Barzegar <Avesta.Barzegar@gmail.com>
2021-10-28 00:49:25 -0400

    fix queries to match models

--- models/database/queries.go
+++ models/database/queries.go
@@ -12,7 +12,7 @@ func GetHistoricalTopArtists(spotify_user_id string) ([]TopArtists, error) {
 		return topArtists, e
 	}
 	conn := db.Conn
-	stmtStr := `SELECT * FROM artists WHERE spotify_user_id=$1`
+	stmtStr := `SELECT created_at, time_span, items FROM artists WHERE spotify_user_id=$1`
 	stmt, err := conn.Prepare(stmtStr)
 	if err != nil {
 		log.Print(err)
@@ -24,7 +24,7 @@ func GetHistoricalTopArtists(spotify_user_id string) ([]TopArtists, error) {
 	defer rows.Close()
 	for rows.Next() {
 		topArtist := new(TopArtists)
-		err := rows.Scan(&topArtist.Id, &topArtist.SpotifyUserId, &topArtist.CreatedAt, &topArtist.TimeSpan, &topArtist.Artists)
+		err := rows.Scan(&topArtist.CreatedAt, &topArtist.TimeSpan, &topArtist.Artists)
 		if err != nil {
 			log.Printf("Something went wrong %v", err)
 		}
@@ -33,5 +33,39 @@ func GetHistoricalTopArtists(spotify_user_id string) ([]TopArtists, error) {
 	if err := rows.Err(); err != nil {
 		log.Printf("Something went wrong %v", err)
 	}
+	log.Println(topArtists)
 	return topArtists, nil
+}
+
+func GetHistoricalTopTracks(spotify_user_id string) ([]TopTracks, error) {
+	db, e := Initialize()
+	var topTracks []TopTracks
+	if e != nil {
+		log.Printf("Something went wrong %v", e)
+		return topTracks, e
+	}
+	conn := db.Conn
+	stmtStr := `SELECT created_at, time_span, items FROM tracks WHERE spotify_user_id=$1`
+	stmt, err := conn.Prepare(stmtStr)
+	if err != nil {
+		log.Print(err)
+	}
+	rows, errStmt := stmt.Query(spotify_user_id)
+	if errStmt != nil {
+		log.Printf("Something went wrong %v", errStmt)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		topTrack := new(TopTracks)
+		err := rows.Scan(&topTrack.CreatedAt, &topTrack.TimeSpan, &topTrack.Tracks)
+		if err != nil {
+			log.Printf("Something went wrong %v", err)
+		}
+		topTracks = append(topTracks, *topTrack)
+	}
+	if err := rows.Err(); err != nil {
+		log.Printf("Something went wrong %v", err)
+	}
+	log.Println(topTracks)
+	return topTracks, nil
 }
